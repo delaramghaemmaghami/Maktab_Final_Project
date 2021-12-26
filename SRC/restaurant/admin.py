4bd5f3a560f601c9d323bc1bd5c16d2ca1188813
DelaramGhaemmaghami <delaramsadatghaemmaghami1997@gmail.com>
2021-12-26 21:49:20 +0330

    Restaurant app has been created with its models

---
+++ SRC/restaurant/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Restaurant)
+admin.site.register(BranchAddress)
+admin.site.register(MealCategory)
+admin.site.register(FoodRestaurantCategory)
+admin.site.register(Branch)
+admin.site.register(Food)
+admin.site.register(Menu)
---
+++ SRC/restaurant/models.py
@@ -0,0 +1,50 @@
+from django.core.validators import MinValueValidator
+from django.db import models
+from accounts.model import Staff
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class BranchAddress(models.Model):
+    city = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+
+
+class MealCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class FoodRestaurantCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Branch(models.Model):
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    is_main = models.BooleanField()
+    description = models.TextField(max_length=500)
+
+    food_restaurant_category = models.OneToOneField(FoodRestaurantCategory, on_delete=models.CASCADE)  # todo: FK
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    branch_address = models.OneToOneField(BranchAddress, on_delete=models.CASCADE)
+    manager = models.OneToOneField(Staff, on_delete=models.CASCADE)
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="media/")
+    description = models.TextField(max_length=500)
+    created = models.DateTimeField(auto_now_add=True)
+
+    meal_category = models.ManyToManyField(MealCategory, related_name="meal")
+    food_restaurant_category = models.ForeignKey(FoodRestaurantCategory, on_delete=models.CASCADE)
+
+
+class Menu(models.Model):
+    inventory = models.PositiveIntegerField()
+    price = models.DecimalField(validators=[MinValueValidator(0.0)])
+
+    food = models.ManyToManyField(Food, related_name="food")
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
