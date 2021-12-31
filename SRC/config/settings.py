23e125e1fca811dc45f64017f20cadee49b7e91e
DelaramGhaemmaghami <delaramsadatghaemmaghami1997@gmail.com>
2021-12-31 21:50:30 +0330

    Adding CSS files and some bootstrap buttons for home page

--- SRC/config/settings.py
+++ SRC/config/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -138,6 +141,5 @@ LOGOUT_REDIRECT_URL = "home"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "media"
-
 
 SITE_ID = 2
