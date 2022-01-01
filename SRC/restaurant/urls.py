0921854c00d9f598b8fdf9bb6df0341cfd4a7224
DelaramGhaemmaghami <delaramsadatghaemmaghami1997@gmail.com>
2022-01-01 14:42:28 +0330

    Food_list according to food_meal_cat will be shown

--- SRC/restaurant/urls.py
+++ SRC/restaurant/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path("food-restaurant-meal-list/", food_restaurant_meal_list, name="food_restaurant_meal_list"),
     path("restaurant-list/", restaurants_list, name="Restaurants_list"),
     path("branches-list/", branches_list, name="branches_list"),
+
     path("food-reataurant-meal-foods-list/<int:food_restaurant_category_id>",
          food_reataurant_meal_foods_list, name="food_reataurant_meal_foods_list"),
+
+    path("meal_category_foods_list/<int:meal_category_id>",
+         meal_category_foods_list, name="meal_category_foods_list"),
 ]
--- SRC/restaurant/views.py
+++ SRC/restaurant/views.py
@@ -35,3 +35,10 @@ def food_reataurant_meal_foods_list(request, food_restaurant_category_id):
     food_restaurant_category = FoodRestaurantCategory.objects.get(id=food_restaurant_category_id)
     data = Food.objects.filter(food_restaurant_category=food_restaurant_category).order_by("-id")
     return render(request, "restaurant/food_reataurant_meal_foods_list.html", {"data": data})
+
+
+# 'food list' according to 'meal cat'
+def meal_category_foods_list(request, meal_category_id):
+    meal_category = MealCategory.objects.get(id=meal_category_id)
+    data = Food.objects.filter(meal_category=meal_category).order_by("-id")
+    return render(request, "restaurant/meal_category_foods_list.html", {"data": data})
