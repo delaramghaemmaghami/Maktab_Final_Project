ecc83a15fa56ca92fb58025c210d845170211928
DelaramGhaemmaghami <delaramsadatghaemmaghami1997@gmail.com>
2022-01-02 13:44:43 +0330

    Overriding signup template

--- SRC/accounts/forms.py
+++ SRC/accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import *
 from django import forms
 from django.db import transaction
@@ -46,3 +46,18 @@ class StaffCreationForm(UserCreationForm):
         user.is_staff = True
         user.save()
         return user
+
+
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': '', 'id': 'hello'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': '',
+            'id': 'hi',
+        }
+    ))
--- SRC/accounts/urls.py
+++ SRC/accounts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth import views
 
 
 urlpatterns = [
     path("signup/", sign_up, name="signup"),
     path("signup/customer/", CustomerSignUpView.as_view(), name="customer_signup"),
-    path("signup/staff/", StaffSignUpView.as_view(), name="staff_signup")
+    path("signup/staff/", StaffSignUpView.as_view(), name="staff_signup"),
+    path('login/',
+         views.LoginView.as_view(template_name="accounts/login.html", authentication_form=UserLoginForm),
+         name="login")
 ]
